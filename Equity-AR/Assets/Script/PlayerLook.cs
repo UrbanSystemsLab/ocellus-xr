4361d6fdc5cd310e1762cbf811b5fcd74d1175ef
Yanzhe Wu <wuyanzhe950606@gmail.com>
2019-12-18 20:26:50 -0500

    Using Heatmap & Greenroof database to generate maps with buildings
    This includes two maps that contains heatmap data and greenroof data. Meanwhile, the maps can generate 3d buildings depand on the data.

---
+++ Equity-AR/Assets/Script/PlayerLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField] private string mouseXInputName, mouseYInputName;
+    [SerializeField] private float mouseSensitivity;
+
+    [SerializeField] private Transform playerBody;
+
+    private void Awake()
+    {
+        LockCursor();
+    }
+
+    private void LockCursor() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Equity-AR/Assets/Script/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float movementSpeed;
+
+    //private CharacterController charController;
+
+    private void Awake()
+    {
+        //charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
+
+        transform.Translate(Vector3.forward * vertInput);
+        transform.Translate(Vector3.right * horizInput);
+        //Vector3 rightMovement = transform.right * horizInput;
+
+
+    }
+
+}
