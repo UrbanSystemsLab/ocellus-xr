4c1098a9d6edc00324ea9aff3f10c5b39e252185
MariaLulu <mariafanpeilu@outlook.com>
2020-12-18 15:27:45 -0500

    TreeColors&InfoDisplay

--- Equity-AR/Assets/Mapbox/Unity/MeshGeneration/Modifiers/GameObjectModifiers/PrefabModifier.cs
+++ Equity-AR/Assets/Mapbox/Unity/MeshGeneration/Modifiers/GameObjectModifiers/PrefabModifier.cs
@@ -1,12 +1,15 @@
 namespace Mapbox.Unity.MeshGeneration.Modifiers
 {
 	using UnityEngine;
+	using UnityEngine.UI;
 	using Mapbox.Unity.MeshGeneration.Data;
 	using Mapbox.Unity.MeshGeneration.Components;
 	using Mapbox.Unity.MeshGeneration.Interfaces;
 	using System.Collections.Generic;
 	using Mapbox.Unity.Map;
 	using System;
+	using System.IO;
+	using System.Text;
 
 	[CreateAssetMenu(menuName = "Mapbox/Modifiers/Prefab Modifier")]
 	public class PrefabModifier : GameObjectModifier
@@ -15,6 +18,19 @@ namespace Mapbox.Unity.MeshGeneration.Modifiers
 		[SerializeField]
 		private SpawnPrefabOptions _options;
 		private List<GameObject> _prefabList = new List<GameObject>();
+		List<string> treetypeList = new List<string>();
+		Dictionary<string, int> treeDict = new Dictionary<string, int>();
+		FileInfo fileInfo;
+		FileStream file;
+		StreamWriter writer;
+		StreamReader reader;
+		public Text treeNumInfo;
+
+		Transform mainCam;
+		int treeNum = 0;
+		
+
+
 
 		public override void Initialize()
 		{
@@ -22,6 +38,13 @@ namespace Mapbox.Unity.MeshGeneration.Modifiers
 			{
 				_objects = new Dictionary<GameObject, GameObject>();
 			}
+			treetypeList.Clear();
+
+			file = new FileStream(Application.dataPath + "/CustomData/treetype.txt", FileMode.Open);
+
+			mainCam = GameObject.Find("Main Camera").GetComponent<Transform>();
+			treeNum = 0;
+
 		}
 
 		public override void SetProperties(ModifierProperties properties)
@@ -52,6 +75,7 @@ namespace Mapbox.Unity.MeshGeneration.Modifiers
 			}
 
 			PositionScaleRectTransform(ve, tile, go);
+			//NearbyTrees(ve, mainCam);
 
 			if (_options.AllPrefabsInstatiated != null)
 			{
@@ -59,11 +83,40 @@ namespace Mapbox.Unity.MeshGeneration.Modifiers
 			}
 		}
 
-		public void PositionScaleRectTransform(VectorEntity ve, UnityTile tile, GameObject go)
+   //     public void NearbyTrees(VectorEntity ve, Transform mainCam)
+   //     {
+			//treeNumInfo = GameObject.Find("TreeNum").GetComponent<Text>();
+   //         Vector3 camPos = mainCam.position;
+
+   //         foreach (var point in ve.Feature.Points[0])
+   //         {
+   //             float dist = Vector3.Distance(camPos, point);
+   //             if (dist < 10)
+   //             {
+   //                 treeNum++;
+   //                 Debug.Log($"Number of trees in an circle of 10 m radius {treeNum}");
+   //             }
+   //         }
+
+			//treeNumInfo.text = "Trees in 10 meters:" + treeNum;
+
+   //     }
+
+        public void PositionScaleRectTransform(VectorEntity ve, UnityTile tile, GameObject go)
 		{
+			
 			RectTransform goRectTransform;
+			Transform stemTransform;
+			Transform crownTransform;
+			GameObject crownType;
 			IFeaturePropertySettable settable = null;
 			var centroidVector = new Vector3();
+
+			float property = 0;
+			float crownR = 0;
+			bool treeFound = false;
+			string treeType = "";
+
 			foreach (var point in ve.Feature.Points[0])
 			{
 				centroidVector += point;
@@ -72,38 +125,96 @@ namespace Mapbox.Unity.MeshGeneration.Modifiers
 
 			go.name = ve.Feature.Data.Id.ToString();
 
-
-			float property = 0;
-			bool treeFound = false;
-
 			//Getting tree DBH and convert it to meters from inch.
 			try
 			{
 				float.TryParse(ve.Feature.Properties["DBH"].ToString(), out property);
-
+				treeType = ve.Feature.Properties["GenusSpeci"].ToString();
 				property = property * 0.0254f;
+				crownR = 0.1f + property * 2;
 				treeFound = true;
+
 			}
 			catch (Exception ex)
 			{
 				property = 0;
+				crownR = 0;
 				treeFound = false;
 				Debug.Log("Temperature layer error:" + ex.Message);
 			}
 
 
+            //if (!treetypeList.Contains(treeType))
+            //{
+            //    treetypeList.Add(treeType);
+            //    WriteMessage(treeType);
+            //}
 
-			goRectTransform = go.GetComponent<RectTransform>();
+			//if (!treeDict.ContainsKey(treeType))
+   //         {
+			//	treeDict.Add(treeType, 1);
+   //         }
+   //         else
+   //         {
+			//	treeDict[treeType]++;
+			//}
+
+
+			//Write the tree type data into local txt file
+			//file.SetLength(0);
+
+			//foreach (KeyValuePair<string, int> kvp in treeDict)
+   //         {
+			//	writer = new StreamWriter(file);
+			//	writer.WriteLine($"{kvp.Key} ; {kvp.Value}");
+			//	writer.Flush();
+   //         }
+
+            goRectTransform = go.GetComponent<RectTransform>();
+			stemTransform = go.gameObject.transform.GetChild(0);
+			crownTransform = go.gameObject.transform.GetChild(1);
+
 			if (goRectTransform == null)
 			{
-
+				 
 				go.transform.localPosition = centroidVector;
 
 				//Set tree height
 				if (treeFound)
 				{
-					go.transform.localPosition = new Vector3(go.transform.localPosition.x, property / 2, go.transform.localPosition.z);
-					go.transform.localScale = new Vector3(0.5f, property / 2, 0.5f);
+					
+
+					stemTransform.transform.localPosition = new Vector3(0, property / 2, 0);
+					stemTransform.transform.localScale = new Vector3(0.1f + property / 4, property, 0.1f + property / 4);
+
+					crownTransform.transform.localScale = new Vector3(crownR, crownR, crownR);
+					crownTransform.transform.localPosition = new Vector3(0.0f, property + crownR / 2, 0.0f);
+
+					
+					switch(treeType)
+                    {
+						case "Gleditsia triacanthos var. inermis - Thornless honeylocust":
+							crownType = crownTransform.GetChild(1).gameObject;
+							break;
+						case "Platanus x acerifolia - London planetree":
+							crownType = crownTransform.GetChild(2).gameObject;
+							break;
+						case "Quercus palustris - pin oak":
+							crownType = crownTransform.GetChild(3).gameObject;
+							break;
+						case "Pyrus calleryana - Callery pear":
+							crownType = crownTransform.GetChild(4).gameObject;
+							break;
+						case "Zelkova serrata -Japanese zelkova":
+							crownType = crownTransform.GetChild(5).gameObject;
+							break;
+						default:
+							crownType = crownTransform.GetChild(0).gameObject;
+							break;
+					}
+					
+					crownType.SetActive(true);
+					
 				}
 
 
---
+++ Equity-AR/Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text treeNumInfo;
+    public GameObject TreeDetection;
+
+    public GameObject TreeInfoPanel;
+    public Text TreeInfo;
+    public GameObject TreeInfoFinder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        treeNumInfo.text = "Trees in 10 meters radius: " + TreeDetection.GetComponent<TreeCount>().treeCount;
+    }
+}
