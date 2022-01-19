0d20a2566b7793560678caa522a388bcc1124ee4
Elena <pengx091@newschool.edu>
2022-01-19 15:36:43 -0500

    srping 2022 update

---
+++ Equity-AR/Assets/Scripts/AskLocation.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AskLocation : MonoBehaviour
+{
+    public static AskLocation Instance
+    {
+        set;
+        get;
+    }
+    public float lat;
+    public float lon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(StartLocationService());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private IEnumerator StartLocationService()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("User has not enable location service.");
+            yield break;
+        }
+
+        Input.location.Start();
+        int maxWait = 20;
+        while(Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if(maxWait <=0)
+        {
+            Debug.Log("Time out");
+            yield break;
+        }
+
+        if(Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Unable to determine device location");
+            yield break;
+        }
+
+        lat = Input.location.lastData.latitude;
+        lon = Input.location.lastData.longitude;
+
+        yield break;
+    }
+}
---
+++ Equity-AR/Assets/Scripts/MessageClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class MessageClass : MonoBehaviour
+{
+    //Create a custom struct and apply [Serializable] attribute to it
+    [Serializable]
+    public struct Message
+    {
+        public float latitude;
+        public float longtitude;
+        public int id;
+        public bool sent;
+    }
+
+
+    [SerializeField]
+    public Message message;
+}
