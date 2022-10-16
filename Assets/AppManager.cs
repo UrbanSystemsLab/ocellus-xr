e8dfa7bc5872e424ee55e0c75c036e1c91f9962f
Larabbit <pengx091@newschool.edu>
2022-10-16 19:02:47 -0400

    Adding App Manager Scene
    - control the flow of the scene switch

---
+++ Assets/AppManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AppManager : MonoBehaviour
+{
+    public static AppManager instance;
+    public GameObject loadingScreen;
+
+    private void Awake()
+    {
+        instance = this;
+
+        SceneManager.LoadSceneAsync((int)SceneIndexes.MENU, LoadSceneMode.Additive);
+    }
+
+    public void LoadApp()
+    {
+        loadingScreen.SetActive(true);
+        SceneManager.UnloadSceneAsync((int)SceneIndexes.MENU);
+        SceneManager.LoadSceneAsync((int)SceneIndexes.AR, LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync((int)SceneIndexes.LIVE, LoadSceneMode.Additive);
+    }
+}
